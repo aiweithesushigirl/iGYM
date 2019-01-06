24e922b4d511229148b74d8f91ca891f0b4aa7dc
wuaiwei <wuaiwei@umich.edu>
2019-01-06 20:21:39 +0800

    changed some style

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,9 +5,6 @@ class FooterComponent extends Component {
 	render() {
 		return (
 			<div className="footer">
-				<div className="container-white">
-					<h1 className="h1">iGYM is supported by</h1>
-				</div>
 				<div className="container">
 					<p className="m-0 small"> &copy; 2018 iGYM Solutions</p>
 				</div>
