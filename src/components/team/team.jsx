a6b307887f7882c6fb9885d77595e0542dda6e1b
wuaiwei <wuaiwei@umich.edu>
2019-01-15 06:49:06 -0500

    change text alignment

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -6,45 +6,56 @@ class TeamComponent extends Component {
 		return (
 			<div className="team">
 				<h1 className="h1">Team</h1>
+
 				<div className="team-photo">
 					<img
 						src={require('/Users/wuaiwei/Desktop/personal-portfolio/src/components/team/Team.jpg')}
 						alt="1"
 					/>
+					<p className="text-center">
+						We are an interdisciplinary team of designers, engineers, and human-computer interaction
+						researchers at the University of Michigan.
+					</p>
 				</div>
-				<p className="text-center">
-					We are an interdisciplinary team of designers, engineers, and human-computer interaction researchers
-					at the University of Michigan.
-				</p>
+
 				<div className="teams">
 					<div className="faculty">
 						<ul style={{ listStyleType: 'none' }}>
-							<li>Faculty</li>
 							<li>
 								Principal Researcher{' '}
-								<a href="https://stamps.umich.edu/people/detail/roland_graf">Roland Graf</a>
+								<a href="https://stamps.umich.edu/people/detail/roland_graf">
+									<b>Roland Graf</b>
+								</a>
 							</li>
 							<li>
 								{' '}
 								Stamps School of Art and Design{' '}
-								<a href="https://stamps.umich.edu/people/detail/sun_young_park">Sun Young Park</a>
+								<a href="https://stamps.umich.edu/people/detail/sun_young_park">
+									<b>Sun Young Park</b>
+								</a>
 							</li>
 							<li>
 								{' '}
 								Stamps School and School of Information{' '}
-								<a href=" https://kim.engin.umich.edu/">Hun Seok Kim</a>
+								<a href=" https://kim.engin.umich.edu/">
+									<b>Hun Seok Kim</b>
+								</a>
 							</li>
 						</ul>
 					</div>
 					<div className="student">
 						<ul style={{ listStyleType: 'none' }}>
-							<li>Current Students</li>
+							<li>
+								<b>Current Students</b>
+							</li>
 							<li>Emma Shpiz</li>
 							<li>Zixuan Li</li>
 							<li>Dashiell Carichner</li>
 						</ul>
 						<ul style={{ listStyleType: 'none' }}>
-							<li>Former Students</li>
+							<li>
+								<b>Former Students</b>
+							</li>
 							<li>Megan Freund</li>
 							<li>Priyanka Raju</li>
 							<li>Yaqian Zhao</li>
@@ -53,7 +64,9 @@ class TeamComponent extends Component {
 					</div>
 					<div className="consultant">
 						<ul style={{ listStyleType: 'none' }}>
-							<li>Consultants</li>
+							<li>
+								<b>Consultants</b>
+							</li>
 							<li>Seth Andrew Warschausky, Ph.D.</li>
 							<li>Betsy Howell, M.S., P.T.</li>
 							<li>Robert C. Ferguson, MHS, OTRL</li>
